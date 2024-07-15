a4a382fe260b78f46e2641ce3672f9f5e051c7f3
Parastoo Seyf Ahamdi <parastoo.ahmadi@sw-motion.cn>
2024-07-15 21:20:36 +0330

    initial commit

---
+++ Cpp_basic_examples/source.cpp
@@ -0,0 +1,11 @@
+// Cpp_basic_examples.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
